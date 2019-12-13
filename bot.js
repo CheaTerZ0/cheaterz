22a7b274ebfc4db5bfe53370592e5c3ccbd9fbca
CheaTerZ0 <58860347+CheaTerZ0@users.noreply.github.com>
2019-12-13 19:53:30 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,6 +13,45 @@ client.on("message", message => {
  message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
  message.delete(); 
 };     
+});client.on('message', message => {
+  if(!message.channel.guild) return;
+if(message.content.startsWith('*bc')) {
+if(!message.channel.guild) return message.channel.send('**this command only for server**').then(m => m.delete(5000));
+if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**sorry you dont have permission** `ADMINISTRATOR`' );
+let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
+let copy = "CheaTerZ Developers ©";
+let request = `Requested By ${message.author.username}`;
+if (!args) return message.reply('**You must write a word or phrase to send the Broadcast**');message.channel.send(`**Are you sure you want to send a Broadcast? \ Broadcast content:** \` ${args}\``).then(msg => {
+msg.react('✅')
+.then(() => msg.react('❌'))
+.then(() =>msg.react('?'))
+
+let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
+let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
+let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
+reaction1.on("collect", r => {
+message.channel.send(`? | Done ... The Broadcast Message Has Been Sent For ${message.guild.members.size} Members`).then(m => m.delete(5000));
+message.guild.members.forEach(m => {
+var bc = new
+Discord.RichEmbed()
+.setColor('RANDOM')
+.setTitle('Broadcast')
+.addField('Server', message.guild.name)
+.addField('Sender', message.author.username)
+.addField('Message', args)
+.setThumbnail(message.author.avatarURL)
+.setFooter(copy, client.user.avatarURL);
+m.send({ embed: bc })
+msg.delete();
+})
+})
+reaction2.on("collect", r => {
+message.channel.send(`**Broadcast Canceled.**`).then(m => m.delete(5000));
+msg.delete();
+})
+})
+}
 });
 
 
@@ -23,7 +62,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : Ar5z ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`S2ud`,"http://twitch.tv/S-F")
+client.user.setGame(`Hypixel|/g join Ar5z`,"http://twitch.tv/S-F")
 client.user.setStatus("dnd")
 });
 
